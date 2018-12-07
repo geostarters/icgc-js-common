244f00be74c0531a6944326bbc4693e1a8300efa
Isaac Besora Vilardaga <isaac@altersport.cat>
2018-12-07 12:13:03 +0100

    Adds two new scripts (build-web and build-react) to better clarify their purpose

--- dist/geo/styleURL.js
+++ dist/geo/styleURL.js
@@ -1,9 +1,13 @@
+//      
+
 // See ICGC's styles in action in https://openicgc.github.io/icgc-basemap.html
-export default StyleURL = {
+const StyleURL = {
 	ICGC: "https://geoserveis.icgc.cat/contextmaps/icgc.json",
 	OSMBright: "https://geoserveis.icgc.cat/contextmaps/osm-bright.json",
 	Positron: "https://geoserveis.icgc.cat/contextmaps/positron.json",
 	Hybrid: "https://geoserveis.icgc.cat/contextmaps/hibrid.json",
 	Dark: "https://geoserveis.icgc.cat/contextmaps/fulldark.json",
 	Night: "https://geoserveis.icgc.cat/contextmaps/night.json"
-}
+};
+
+export default StyleURL;
