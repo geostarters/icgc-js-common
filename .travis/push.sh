c8c78839c581e5f3025c7039bcf940a7e4044c19
Isaac Besora Vilardaga <isaac@altersport.cat>
2019-02-01 13:50:05 +0100

    New travis commands

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-git add package.json
-git commit -m "Travis - Update package.json version"
-git push https://geostarters:${GITHUB_TOKEN}@github.com/geostarters/icgc-js-common.git
 git push --tags https://geostarters:${GITHUB_TOKEN}@github.com/geostarters/icgc-js-common.git
