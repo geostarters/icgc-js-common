40ddd6a17ccff768d344be9dd15922a54384d679
Isaac Besora Vilardaga <isaac@altersport.cat>
2019-02-01 16:27:06 +0100

    Moves push to github to another script

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+# Push the commit with the updated package.json back to the repo
+git branch travis-temp
+git checkout master
+git merge travis-temp
+git push https://geostarters:${GITHUB_TOKEN}@github.com/geostarters/icgc-js-common.git
 git push --tags https://geostarters:${GITHUB_TOKEN}@github.com/geostarters/icgc-js-common.git
