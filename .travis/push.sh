cb2a6b7a99c843eae46c172cc312e80e0dc2487e
Isaac Besora Vilardaga <isaac@altersport.cat>
2019-02-01 13:38:25 +0100

    Moved git functions to aux script

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
-setup_git() {
-  git remote add origin https://geostarters:${GH_TOKEN}@github.com/geostarters/icgc-js-common.git
-}
+git add package.json
+git commit -m "Travis - Update package.json version"
+git push https://geostarters:${GITHUB_TOKEN}@github.com/geostarters/icgc-js-common.git
+git push --tags https://geostarters:${GITHUB_TOKEN}@github.com/geostarters/icgc-js-common.git
